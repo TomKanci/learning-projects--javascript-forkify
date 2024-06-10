ab0ca10a3556be7e3846dd507c5c9069394ebcd4
Tomas Kancian <tomas.kancian@iqvia.com>
2024-06-10 09:30:54 +0200

    Keep only the build from parcel

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
-export const API_KEY = "09eccd8c-fd3c-437d-8e00-f7780e2159bb";
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const MODAL_CLOSE_SEC = 2.5;
