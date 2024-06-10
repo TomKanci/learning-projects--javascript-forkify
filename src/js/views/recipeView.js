8ac4004a285edfe0ea490b2f26886361350896d1
Tomas Kancian <tomas.kancian@iqvia.com>
2024-06-10 11:49:46 +0200

    Replace 'fractional' with 'fracty'

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from "./View.js";
 
 import icons from "../../img/icons.svg";
-import { Fraction } from "fractional";
+import fracty from "fracty";
 
 class RecipeView extends View {
   _parentEl = document.querySelector(".recipe");
@@ -134,7 +134,7 @@ class RecipeView extends View {
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ""
+          ing.quantity ? fracty(ing.quantity).toString() : ""
         }</div>
         <div class="recipe__description">
           ${ing.unit ? `<span class="recipe__unit">${ing.unit}</span>` : ""}
